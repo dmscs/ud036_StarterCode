cc9d998b9ac861bb49e17b411a26a4a5d0b24744
rick <sml.hng@gmail.com>
2017-05-19 10:23:49 -0700

    Added movie class for data structure + list of pixar movies

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 
 class Movie():
+	'''This class provides a way to store movie related information'''
 	def __init__(self, title, poster_image_url, trailer_youtube_url):
 		self.title = title
 		self.poster_image_url = poster_image_url
